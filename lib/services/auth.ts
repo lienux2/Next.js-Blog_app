1738516ad10daa23fc02617aff8ad3b33bc2f342
lienux2 <lienesvilpe5@gmail.com>
2024-02-12 13:43:46 +0200

    Few changes

--- lib/services/auth.ts
+++ lib/services/auth.ts
@@ -1,43 +1,43 @@
-import { compare } from "bcrypt";
-import { connectMongoDB } from "@(lib)/mongo/connect";
-import UserModel, { SomeUser } from "@(lib)/models/User";
+// import { compare } from "bcrypt";
+// import { connectMongoDB } from "@(lib)/mongo/connect";
+// import UserModel, { SomeUser } from "@(lib)/models/User";
 
-interface UserLoginData {
-  username: string;
-  password: string;
-}
+// interface UserLoginData {
+//   username: string;
+//   password: string;
+// }
 
-export const loginUser = async ({
-  username,
-  password,
-}: UserLoginData): Promise<SomeUser | null> => {
-  await connectMongoDB();
+// export const loginUser = async ({
+//   username,
+//   password,
+// }: UserLoginData): Promise<SomeUser | null> => {
+//   await connectMongoDB();
 
-  if (!username || !password) {
-    console.log("data not entered");
-    throw new Error("Username or password not entered");
-  }
+//   if (!username || !password) {
+//     console.log("data not entered");
+//     throw new Error("Username or password not entered");
+//   }
 
-  const user: SomeUser | null = await UserModel.findOne({
-    username,
-  });
+//   const user: SomeUser | null = await UserModel.findOne({
+//     username,
+//   });
 
-  if (!user) {
-    console.log("data not exist");
-    throw new Error("This username does not exist");
-  }
+//   if (!user) {
+//     console.log("data not exist");
+//     throw new Error("This username does not exist");
+//   }
 
-  if (user.password) {
-    const isValid = await compare(password, user.password);
-    console.log(user.password);
+//   if (user.password) {
+//     const isValid = await compare(password, user.password);
+//     console.log(user.password);
 
-    if (!isValid) {
-      console.log("data not correct");
-      throw new Error("This password is not correct");
-    }
-  } else {
-    throw new Error("Please check again your username and password.");
-  }
+//     if (!isValid) {
+//       console.log("data not correct");
+//       throw new Error("This password is not correct");
+//     }
+//   } else {
+//     throw new Error("Please check again your username and password.");
+//   }
 
-  return user;
-};
+//   return user;
+// };
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,23 +1,23 @@
-import NextAuth from "next-auth";
-import User from "@/lib/models/User";
-import { JWT } from "next-auth/jwt";
+// import NextAuth from "next-auth";
+// import User from "@/lib/models/User";
+// import { JWT } from "next-auth/jwt";
 
-declare module "next-auth" {
-  interface Session {
-    user: User & DefaultSession["user"];
-  }
+// declare module "next-auth" {
+//   interface Session {
+//     user: User & DefaultSession["user"];
+//   }
 
-  interface User {
-    _id?: string;
-    username: string;
-    password: string;
-    createdAt: string;
-    updatedAt: string;
-  }
-}
+//   interface User {
+//     _id?: string;
+//     username: string;
+//     password: string;
+//     createdAt: string;
+//     updatedAt: string;
+//   }
+// }
 
-declare module "next-auth/jwt" {
-  interface JWT {
-    user: User & DefaultSession["user"];
-  }
-}
+// declare module "next-auth/jwt" {
+//   interface JWT {
+//     user: User & DefaultSession["user"];
+//   }
+// }
